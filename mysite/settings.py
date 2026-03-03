de17fb31170a12dad0ab273b8d0f734b822619e4
goode8 <drgood@umich.edu>
2026-03-02 20:31:42 -0800

    add radar map

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ.get("SECRET KEY", ""),)
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 ALLOWED_HOSTS = [h.strip() for h in os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')]
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = os.environ.get('STATIC_URL')
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
 # This tells Django to put collected files in a folder named 'staticfiles' in your project root
 STATIC_ROOT = BASE_DIR / "staticfiles"
